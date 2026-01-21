f886a5c422539d3174f2a7128815aa01ed1a4922
Mayank Sharma <132637383+MAYANKSHARMA01010@users.noreply.github.com>
2026-01-21 11:38:51 +0530

    made it responvie

--- script.js
+++ script.js
@@ -29,6 +29,33 @@ themeToggleBtn.addEventListener('click', () => {
     updateIcon(newTheme);
 });
 
+const mobileBtn = document.getElementById('mobile-menu-toggle');
+const navLinks = document.querySelector('.nav-links');
+
+if (mobileBtn) {
+    mobileBtn.addEventListener('click', () => {
+        navLinks.classList.toggle('active');
+        const icon = mobileBtn.querySelector('i');
+        if (navLinks.classList.contains('active')) {
+            icon.classList.remove('fa-bars');
+            icon.classList.add('fa-times');
+        } else {
+            icon.classList.remove('fa-times');
+            icon.classList.add('fa-bars');
+        }
+    });
+
+
+    navLinks.querySelectorAll('a').forEach(link => {
+        link.addEventListener('click', () => {
+            navLinks.classList.remove('active');
+            const icon = mobileBtn.querySelector('i');
+            icon.classList.remove('fa-times');
+            icon.classList.add('fa-bars');
+        });
+    });
+}
+
 
 const observerOptions = {
     root: null,
